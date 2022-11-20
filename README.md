e43ecdf7ae2853ccb348f2f00eb50e56959114d4
rygo6 <rygo6@me.com>
2022-11-19 16:07:36 -0800

    Cleanup, add readme and license

---
+++ README.md
@@ -0,0 +1,9 @@
+# FabriceNode-Wasmtime-ANGLE
+
+This a prototype showing how to issue rendering calls directly from a Standalone WASM VM to WebGL without needing any JavaScript.
+
+This was initially intended to be an ANGLE+Wasmtime based Node for Fabric, but decided this is not the right route for that.
+
+This contains index.wasm which was compiled via Emscripten and is the bare necessity to render a single triangle. The Wasmtime VM is placed on top of Google Angle. The Wasmtime VM then loads the index.wasm binary and utilizes the C-API to forward the WebGL rendering calls directly from WASM to Google Angle. This route I believe holds promise for something as through the C-API you can send memory directly from the WASM linear memory space into Google Angle for rendering.
+
+Currently builds on windows ith mingw gcc. All necessary dlls are in the third_party folder. But you may have to copy them next to the built exe.
--- src/main.c
+++ src/main.c
@@ -1,35 +1,19 @@
 #include "wasm_context.h"
 #include "render_context.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 em_wasm_context_data_t *wasm_data;
-//ESContext *es_context;
-
-bool run_once = false;
 
 void Draw(ESContext *es_context) {
+// for debugging
+//    DrawTriangle(es_context);
 
-    if (!run_once){
-        run_once = true;
+    EmWasmCallMainLoop(wasm_data);
 
-//        DrawTriangle(es_context);
-
-        EmWasmCallMainLoop(wasm_data);
-
-        eglSwapBuffers(es_context->eglDisplay, es_context->eglSurface);
-    }
-
+    eglSwapBuffers(es_context->eglDisplay, es_context->eglSurface);
 }
 
 int main(int argc, char *argv[]) {
-
-    unsigned int i = 1;
-    char *c = (char*)&i;
-    if (*c)
-        printf("Little Endian\n");
-    else
-        printf("Big Endian\n");
 
     wasm_data = CreateEmWasmContext();
 
@@ -42,10 +26,6 @@ int main(int argc, char *argv[]) {
     esCreateWindow(wasm_data->es_context, "Fabric", 640, 480, ES_WINDOW_RGB);
 
     esRegisterDrawFunc(wasm_data->es_context, Draw);
-
-//    if (!render_context_init(wasm_data->es_context)) {
-//        fprintf(stderr, "Could not init render content. \n");
-//    }
 
     EmWasmCallStart(wasm_data);
 }
